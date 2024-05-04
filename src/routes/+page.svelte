40d30e7b3745763ea6a9a21c63559dab7b77b2c2
Jezreel <jezreeljoshuam@gmail.com>
2024-05-04 14:04:40 +0800

    initial commi

---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<div class="flex flex-row p-2 w-screen justify-center mt-32 gap-36 grow">
+	<div>
+		<h1 class="font-bold text-9xl mb-6">Jezreel Joshua Martin</h1>
+		<h3 class="text-7xl font-semibold">@<span class="underline">JezReal</span></h3>
+
+		<div class="mt-36">
+			<p class="text-xl text-wrap">
+				A motivated and curiosity driven software engineer who has a strong interest in back-end
+				development. In his spare time, he likes learning about the various techonologies and trends
+				in the software field. He also likes to look for ways to improve and grow as a developer.
+			</p>
+		</div>
+	</div>
+	<div class="grow">
+		<enhanced:img src="../lib/assets/images/face.jpg" alt="Jezreel" class="rounded-full">
+		</enhanced:img>
+	</div>
+</div>
+
+<style>
+	img {
+		height: 650px;
+		width: 650px;
+	}
+
+	h1 {
+		color: #a4a4a4;
+	}
+
+	h3 {
+		color: #d9d9d9;
+	}
+
+	p {
+		color: #868686;
+	}
+</style>
